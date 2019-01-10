0a72dc34485be892ac73271348baf21db93839e0
seaofgreen10 <40816493+seaofgreen10@users.noreply.github.com>
2019-01-09 22:40:02 -0700

    Update board_fetch_runner.py

--- board_fetch_runner.py
+++ board_fetch_runner.py
@@ -1,13 +1,14 @@
 #import api_interface.api_calls as api
 #import api_interface.api_data_parser as parser
 import db_accessor as db
+import time
 
 def main():
     ctr=0
     while(ctr<100):
         print("hi")
         ctr+=1
-        sleep(30)
+        time.sleep(10)
 
 main()
 #db.add_to_db("nate", 20, 2, "2019-01-03", "00:00:00")
